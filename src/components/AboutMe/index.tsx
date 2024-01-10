d6cd257d311a4c70138904104b66ed859531ba94
M4rcosz <ijohnfelipe71@gmail.com>
2024-01-10 00:55:00 -0300

    Reestruturando e alterando o favicon

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -2,15 +2,14 @@
 const AboutMe = () => {
 
     return (
-        <section className="flex flex-col gap-8 grow justify-center mb-60 relative h-full">
-            <div className="w-full h-screen bg-cover bg-no-repeat blur-[0.2px] absolute z-[-1] bg-center"> </div>
-            <h1 className="text-center absolute top-4 translate-x-1/2 right-1/2 font-rubikDoodle font-semibold text-white uppercase">Biografia</h1>
-            <div className="overflow-hidden rounded-full bg-white w-40 h-40 mx-auto border-2 box-content">
+        <section className="flex grow justify-center mb-60 h-fit">
+            <div className="flex overflow-hidden rounded-full w-[160px] h-[160px]  border-[3px]  shrink-0">
                 <img src="minhaFoto.jpg" alt="Foto de Marcos Paulo" className="w-full h-full" />
             </div>
-            <p className="text-center mx-16 ">
-                Opa, me chamo Marcos Paulo, e sou um jovem autodidata de 18 anos que segue no começo de sua carreira como Front-End Developer, uma pessoa tranquila que deseja mergulhar nesse mundo da programação e aprender coisas novas, um amante da tecnologia que um dia deseja contribuir para novas ferramentas e inovações no mundo.
-            </p>
+            <span className="flex flex-col gap-2 mx-8 text-white">
+                <h1 className="text-white text-4xl font-quickSand font-semibold ">Marcos Paulo</h1>
+                <p>Sou um jovem autodidata que segue no começo de sua carreira como Front-End Developer, uma pessoa que deseja mergulhar nesse mundo da programação e aprender coisas novas, um amante da tecnologia que um dia deseja contribuir para novas ferramentas e inovações no mundo.</p>
+            </span>
         </section>
     )
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
     return (
         <nav className="flex m-4 justify-end">
-            <div className='flex w-10 flex-wrap cursor-pointer gap-[2px] min-[1440px]:w-12'>
+            {/* <div className='flex w-10 flex-wrap cursor-pointer gap-[2px] min-[1440px]:w-12'>
 
                 <div className='border-black border border-[2px] rounded-full w-[9px] h-[9px] border-[#000000f0] min-[1440px]:w-[14px] min-[1440px]:h-[14px] '></div>
                 <div className='border-black border border-[2px] rounded-full w-[9px] h-[9px] border-[#000000f0] min-[1440px]:w-[14px] min-[1440px]:h-[14px] '></div>
@@ -14,7 +14,7 @@ const NavBar = () => {
                 <div className='border-black border border-[2px] rounded-full w-[9px] h-[9px] border-[#000000f0] min-[1440px]:w-[14px] min-[1440px]:h-[14px] '></div>
                 <div className='border-black border border-[2px] rounded-full w-[9px] h-[9px] border-[#000000f0] min-[1440px]:w-[14px] min-[1440px]:h-[14px] '></div>
                 <div className='border-black border border-[2px] rounded-full w-[9px] h-[9px] border-[#000000f0] min-[1440px]:w-[14px] min-[1440px]:h-[14px] '></div>
-            </div>
+            </div> */}
 
             {/* <ul className="flex gap-12 basis-1/2 ">
                 <li>
